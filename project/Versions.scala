64bb3ddbfa5d37f17d344c595a262470aeaa702b
SemanticBeeng <nick@semanticbeeng.com>
2018-04-28 04:35:06 -0700

    build update

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,15 +2,15 @@
   * Created by boris on 7/14/17.
   */
 object Versions {
-  val reactiveKafkaVersion = "0.16"
-  val akkaVersion = "2.4.19"
+  val reactiveKafkaVersion = "0.20"
+  val akkaVersion = "2.5.11"
   val akkaSlfVersion = "2.3.2"               // older version to align with Flink
-  val akkaHttpVersion = "10.0.9"
+  val akkaHttpVersion = "10.1.0"
   val akkaHttpJsonVersion = "1.17.0"
 
   val Curator        = "3.2.0"
   val kafkaVersion = "0.10.2.1"
-  val tensorflowVersion = "1.1.0"
+  val tensorflowVersion = "1.7.0"
   val PMMLVersion = "1.3.5"
   val jettyVersion = "9.2.12.v20150709"
   val jacksonVersion = "2.8.8"
